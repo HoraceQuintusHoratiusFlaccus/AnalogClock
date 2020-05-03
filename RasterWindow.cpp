8389a1b04df941db4ac9f32cb48c61ec3b0fb071
Cho-yeon <HoraceQuintusHoratiusFlaccus@gmail.com>
2020-05-04 00:13:37 +0900

    CREATE: RasterWindow.cpp

---
+++ RasterWindow.cpp
@@ -0,0 +1,59 @@
+#include "RasterWindow.h"
+
+RasterWindow::RasterWindow(QWindow * Parent)
+    : QWindow(Parent)
+    , BackingStore(new QBackingStore(this))
+{
+    setGeometry(100, 100, 300, 200);
+}
+
+void RasterWindow::exposeEvent(QExposeEvent *)
+{
+    if (isExposed())
+        RenderNow();
+}
+
+void RasterWindow::resizeEvent(QResizeEvent * resizeEvent)
+{
+    BackingStore->resize(resizeEvent->size());
+}
+
+void RasterWindow::RenderNow()
+{
+    if (!isExposed())
+        return;
+
+    QRect Rect(0, 0, width(), height());
+    BackingStore->beginPaint(Rect);
+
+    QPaintDevice * Device = BackingStore->paintDevice();
+    QPainter Painter(Device);
+
+    Painter.fillRect(0, 0, width(), height(), QGradient::NightFade);
+    Render(&Painter);
+    Painter.end();
+
+    BackingStore->endPaint();
+    BackingStore->flush(Rect);
+}
+
+void RasterWindow::Render(QPainter * Painter)
+{
+    /* Ex) Painter->drawText(QRectF(0, 0, width(), height()), Qt::AlignCenter, QStringLiteral("QWindow")); */
+}
+
+void RasterWindow::RenderLater()
+{
+    requestUpdate();
+}
+
+bool RasterWindow::event(QEvent * Event)
+{
+    if (Event->type() == QEvent::UpdateRequest)
+    {
+        RenderNow();
+        return true;
+    }
+
+    return QWindow::event(Event);
+}
